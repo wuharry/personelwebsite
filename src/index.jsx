a65601f35e059f8e3e53a2663a1cd4ed91192787
wuharry <whw880218cool@gmail.com>
2024-04-03 01:18:46 +0800

    build:更新package
    1.add vite
    2.remove react-script(套件兩年沒更新了)
    3.remove tailwindcss,以目前來看該專案都是用scss做的

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./pages/App";
+import { BrowserRouter as Router } from "react-router-dom";
+function App() {
+  root.render(
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>
+  );
+}
