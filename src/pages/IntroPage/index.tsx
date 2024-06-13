88ba83ca00da2be3e4f3a0b990cee8d90cf34f56
wuharry <whw880218cool@gmail.com>
2024-06-14 04:44:28 +0800

    fix:change 104 logo and cakeResume logo

--- src/pages/IntroPage/index.tsx
+++ src/pages/IntroPage/index.tsx
@@ -1,6 +1,7 @@
 /** @format */
 import React, { useEffect, useState } from "react";
 import { Button, NavigationBar, IconButton, HeaderIcon } from "../../compoment";
+import { Icon104, CakeResume } from "../../static/constant/svg";
 import clsx from "clsx";
 
 const Intro: React.FC = () => {
@@ -64,20 +65,21 @@ const Intro: React.FC = () => {
     {
       name: "CakeResume",
       SVGElement: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-          />
-        </svg>
+        // <svg
+        //   xmlns="http://www.w3.org/2000/svg"
+        //   fill="none"
+        //   viewBox="0 0 24 24"
+        //   strokeWidth="1.5"
+        //   stroke="currentColor"
+        //   className="w-6 h-6"
+        // >
+        //   <path
+        //     strokeLinecap="round"
+        //     strokeLinejoin="round"
+        //     d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+        //   />
+        // </svg>
+        <CakeResume></CakeResume>
       ),
       link: "https://www.cakeresume.com/dashboard",
     },
@@ -98,22 +100,7 @@ const Intro: React.FC = () => {
     },
     {
       name: "104Bank",
-      SVGElement: (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="w-6 h-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
-          />
-        </svg>
-      ),
+      SVGElement: <Icon104></Icon104>,
       link: "https://pda.104.com.tw/profile/share/8sw2sYtjxtWwHu7cKUoGcgPZdo9ocnmu",
     },
   ];
