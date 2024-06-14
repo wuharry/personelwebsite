f1cbfe0d76b355dd6ff9f7daab9b674b5bbbb4f6
wuharry <whw880218cool@gmail.com>
2024-06-14 23:32:17 +0800

    build:fix build fail in github---test(1)

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,13 @@
 
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import sassDts from "vite-plugin-sass-dts";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig((configEnv) => {
   // const isDevelopment = configEnv.mode === "development";
 
   return {
-    plugins: [react(), sassDts(), svgr()],
+    plugins: [react(), svgr()],
     rules: [
       // 其他规则...
       {
