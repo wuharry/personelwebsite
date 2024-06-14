2aa19fe33ee8ccb33afcf58f75fd524743969b5d
wuharry <whw880218cool@gmail.com>
2024-06-14 23:04:13 +0800

    test:Add Project Page

--- src/pages/Project/index.tsx
+++ src/pages/Project/index.tsx
@@ -1,15 +1,27 @@
-import React from "react";
-import { WorkList } from "../../compoment/WorkList";
-import { WORKS } from "../works";
-import { WORKPROPS } from "../../types";
+/** @format */
 
-export const WorkPage: React.FC<WORKPROPS> = () => {
+import { FunctionComponent, useEffect, useState } from "react";
+import { NavigationBar } from "../../compoment";
+import ProjectCard from "../../compoment/ProjectCard";
+import clsx from "clsx";
+import { Project } from "../../compoment/ProjectCard/type";
+import day1 from "../../assets/day1.png";
+
+const WorkPage: FunctionComponent = () => {
   return (
-    <div className={`wrap`}>
-      <div className={`title`}>
-        <h3>作品集</h3>
-      </div>
-      <WorkList dataSource={WORKS} />
+    <div className="flex bg-slate-700 flex-col items-start justify-start w-screen !h-auto min-h-screen overflow-y-scroll">
+      <NavigationBar />
+      <section className={clsx("flex w-full mt-16 mb-10 space-y-10 px-4")}>
+        <ProjectCard
+          className=""
+          label="Drum Kit"
+          description=""
+          image={day1}
+          link="https://wuharry.github.io/day1/"
+        />
+      </section>
     </div>
   );
 };
+
+export default WorkPage;
--- src/pages/Project/works.module.scss.d.ts
+++
@@ -1,5 +0,0 @@
-declare const classNames: {
-  readonly wrap: "wrap";
-  readonly title: "title";
-};
-export = classNames;
