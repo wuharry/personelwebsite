532965cb5435cbb854a61f6add5d6aed2dc2789d
wuharry <whw880218cool@gmail.com>
2024-06-20 01:44:36 +0800

    feat:Add workCard

--- src/pages/Project/index.tsx
+++ src/pages/Project/index.tsx
@@ -7,18 +7,31 @@ import clsx from "clsx";
 import { Project } from "../../compoment/ProjectCard/type";
 import day1 from "../../assets/day1.png";
 
+const PROJECT_LIST: Project[] = [
+  {
+    label: "Drum Kit",
+    link: "https://wuharry.github.io/day1/",
+    description: "js 的 DOM 練習,當按下按鍵的時候",
+    image: day1,
+    className: "",
+  },
+];
+
 const WorkPage: FunctionComponent = () => {
   return (
     <div className="flex bg-slate-700 flex-col items-start justify-start w-full h-full min-h-screen overflow-y-scroll">
       <NavigationBar />
       <section className={clsx("flex w-full mt-16 mb-10 space-y-10 px-4")}>
-        <ProjectCard
-          className=""
-          label="Drum Kit"
-          description=""
-          image={day1}
-          link="https://wuharry.github.io/day1/"
-        />
+        {PROJECT_LIST.map((project) => (
+          <ProjectCard
+            key={`${project.label}-${project.link}`}
+            className={project.className}
+            label={project.label}
+            description={project.description}
+            image={project.image}
+            link={project.link}
+          />
+        ))}
       </section>
     </div>
   );
