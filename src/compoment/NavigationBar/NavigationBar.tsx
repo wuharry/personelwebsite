6c4aea30acf303fb83fde949df4db36c033a695e
wuharry <whw880218cool@gmail.com>
2024-06-23 02:18:47 +0800

    feat:add side bar animetion

--- src/compoment/NavigationBar/NavigationBar.tsx
+++ src/compoment/NavigationBar/NavigationBar.tsx
@@ -2,8 +2,10 @@
 
 import React, { FunctionComponent, useEffect, useState } from "react";
 import clsx from "clsx";
-import { Link } from "react-router-dom"; //git hub 需要hash router
+import { Link } from "react-router-dom";
+
 interface NavigationBarProps {}
+
 const ROUTER_LIST = [
   {
     name: "Home",
@@ -30,34 +32,34 @@ const ROUTER_LIST = [
     link: "/contact",
   },
 ];
-const LINK_STYLE: string = `inline-block text-2xl text-white 
-text-decoration-none font-medium ml-9 transition duration-300
-hover:text-[#0ef]
-  `;
+
+const LINK_STYLE: string = `
+  inline-block text-2xl text-white text-decoration-none font-medium ml-9 
+  transition duration-300 hover:text-[#0ef]
+`;
 
 const NavigationBar: FunctionComponent<NavigationBarProps> = () => {
-  const [navBarAnimetion, setNavBarAnimetion] = useState(false);
-  const [hamburgerAnimetion, setHamburgerAnimetion] = useState(false);
+  const [navBarAnimation, setNavBarAnimation] = useState(false);
+  const [hamburgerAnimation, setHamburgerAnimation] = useState(false);
 
   useEffect(() => {
-    setNavBarAnimetion(true);
-    console.log("init");
+    setNavBarAnimation(true);
 
     return () => {
-      setTimeout(() => setNavBarAnimetion(false), 5000);
+      setTimeout(() => setNavBarAnimation(false), 5000);
     };
   }, []);
 
   const handleHamburgerClick = () => {
-    setHamburgerAnimetion(!hamburgerAnimetion);
-    console.log(hamburgerAnimetion);
+    setHamburgerAnimation(!hamburgerAnimation);
   };
 
   return (
     <>
       <header
         className={clsx(
-          "fixed sm:top-0 sm:left-0 top-0 w-full px-5 py-3 bg-transparent z-10 flex items-center ",
+          "fixed top-0 w-full px-5 py-3 bg-transparent z-20 flex items-center transition-all",
+          hamburgerAnimation ? "bg-cyan-900 w-full h-full" : "",
           "sm:justify-between justify-start"
         )}
       >
@@ -74,7 +76,7 @@ const NavigationBar: FunctionComponent<NavigationBarProps> = () => {
         <nav
           className={clsx(
             "sm:flex sm:justify-around sm:items-center sm:gap-4",
-            "hidden"
+            "hidden sm:flex"
           )}
         >
           {ROUTER_LIST.map((item, index) => (
@@ -82,18 +84,17 @@ const NavigationBar: FunctionComponent<NavigationBarProps> = () => {
               key={`${index}-${item.name}`}
               to={item.link}
               className={clsx(
-                `${LINK_STYLE} ${
-                  navBarAnimetion
-                    ? `animate-fade-down animate-once animate-ease-in`
-                    : ``
-                }`
+                LINK_STYLE,
+                navBarAnimation
+                  ? "animate-fade-down animate-once animate-ease-in"
+                  : ""
               )}
             >
               {item.name}
             </Link>
           ))}
         </nav>
-        <div className={clsx("sm:hidden", "block")}>
+        <div className={clsx("sm:hidden block")}>
           <label
             className={clsx(
               "before:content-[''] before:w-bar-width before:h-bar-height before:bg-slate-400 before:rounded-full",
@@ -103,44 +104,52 @@ const NavigationBar: FunctionComponent<NavigationBarProps> = () => {
               "flex flex-col gap-hamburger-gap w-max absolute top-[0.2rem] left-[0.2rem] cursor-pointer",
               "has-[:checked]:after:box-border has-[:checked]:before:box-border",
               "before:transition-all after:transition-all",
-              "has-[:checked]:before:rotate-45 has-[:checked]:after:rotate-[-45deg]",
-              "has-[:checked]:before:w-hamburger-close-width has-[:checked]:after:w-hamburger-close-width",
-              "has-[:checked]:before:translate-x-0 has-[:checked]:after:translate-x-0",
-              "has-[:checked]:before:translate-y-close-bar-height-before has-[:checked]:after:translate-y-close-bar-height-after",
-              "has-[:checked]:before:transition-all ease-in-out duration-200 has-[:checked]:after:transition-all"
+              hamburgerAnimation
+                ? "before:rotate-45 after:rotate-[-45deg]"
+                : "",
+              hamburgerAnimation
+                ? "before:w-hamburger-close-width after:w-hamburger-close-width"
+                : "",
+              hamburgerAnimation
+                ? "before:translate-x-0 after:translate-x-0"
+                : "",
+              hamburgerAnimation
+                ? "before:translate-y-close-bar-height-before after:translate-y-close-bar-height-after"
+                : "",
+              hamburgerAnimation
+                ? "before:transition-all ease-in-out duration-200 after:transition-all"
+                : ""
             )}
           >
             <input
               type="checkbox"
               name="hamburger-button"
               id="hamburger-button"
-              onClick={handleHamburgerClick}
-              checked={hamburgerAnimetion}
+              onChange={handleHamburgerClick}
+              checked={hamburgerAnimation}
               className={clsx(
                 "content-[''] w-bar-width h-bar-height bg-slate-400 rounded-full appearance-none",
-                "p-0 m-0 outline-none	pointer-events-none transition-opacity ease-in-out duration-200 ",
-
-                hamburgerAnimetion ? "opacity-0 w-0" : "opacity-100 w-bar-width"
+                "p-0 m-0 outline-none pointer-events-none transition-opacity ease-in-out duration-200",
+                hamburgerAnimation ? "opacity-0 w-0" : "opacity-100 w-bar-width"
               )}
             />
           </label>
-          <aside>
-            <nav
-              className={clsx(
-                "flex justify-around items-center gap-4 flex-col",
-                "sm:hidden "
-              )}
-            >
+          <aside
+            className={clsx(
+              "transition-transform",
+              hamburgerAnimation ? "translate-x-0" : "-translate-x-full"
+            )}
+          >
+            <nav className="flex flex-col gap-4 w-full h-full">
               {ROUTER_LIST.map((item, index) => (
                 <Link
                   key={`${index}-${item.name}`}
                   to={item.link}
                   className={clsx(
-                    `${LINK_STYLE} ${
-                      navBarAnimetion
-                        ? `animate-fade-down animate-once animate-ease-in`
-                        : ``
-                    }`
+                    LINK_STYLE,
+                    navBarAnimation
+                      ? "animate-fade-down animate-once animate-ease-in"
+                      : ""
                   )}
                 >
                   {item.name}
