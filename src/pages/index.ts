0827831dae5de002f2cc84de5cc7d41735267163
wuharry <whw880218cool@gmail.com>
2026-02-25 10:39:11 +0800

    chore(build): build project

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,7 +2,7 @@
 import AboutMe from './AboutMe';
 import Contact from './Contact';
 import Experience from './Experience';
-import Intro from './IntroPage';
+import Intro from './Intro';
 import WorkPage from './Project';
 import Skill from './Skill';
 
